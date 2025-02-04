555fa00b2214e9a32dd795f4aef980b361a43449
Hal Ras <rasulhalilov@gmail.com>
2025-02-04 21:54:26 +0300

    add brain-calc.js, calc.js, update README.md

---
+++ src/games/calc.js
@@ -0,0 +1,32 @@
+import mainEngine from '../index.js';
+import getRandomNumber from '../random.js';
+
+const description = 'What is the result of the expression?';
+
+const calculate = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return null;
+  }
+};
+
+const getData = () => {
+  const num1 = getRandomNumber(1, 50);
+  const num2 = getRandomNumber(1, 50);
+  const operators = ['+', '-', '*'];
+  const operator = operators[getRandomNumber(0, operators.length - 1)];
+  console.log(operator);
+  const question = `${num1} ${operator} ${num2}`;
+  const correctAnswer = String(calculate(num1, num2, operator));
+  return [question, correctAnswer];
+};
+
+const startCalc = () => mainEngine(getData, description);
+
+export default startCalc;
