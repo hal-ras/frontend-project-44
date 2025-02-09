fb957f1bdc764c3a88dc63386eb45842b9d3d929
Hal Ras <rasulhalilov@gmail.com>
2025-02-10 00:31:09 +0300

    Revert "fix calc.js, random.js, cli.js, package.json, brain-games.js"
    This reverts commit b649cf5a651502dfbd2ce5c929be7bbfeb0bbe95.

--- src/random.js
+++ src/random.js
@@ -1,4 +1,7 @@
-const getRandomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
+const getRandomNumber = (min, max) => {
+  const random = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return random;
+};
 
 export default getRandomNumber;
