b7dd14263128f41cfc9e5b16193b04db18264c75
hal-ras <rasulhalilov@gmail.com>
2025-02-10 19:17:53 +0300

    fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,11 +1,10 @@
 import mainEngine from '../index.js';
 import getRandomNumber from '../random.js';
 
-const description =
-  'Answer "yes" if given number is prime. Otherwise answer "no".';
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num; i++) {
+  for (let i = 2; i < num; i += 1) {
     if (num % i === 0) return false;
   }
   return num !== 1;
